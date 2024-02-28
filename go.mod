715577408f62c999b732a5d0c45108cb21c9d4b1
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-27 18:36:39 -0800

    add process receipts logic

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module receipt-project-fetch
 go 1.21.4
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/labstack/echo/v4 v4.11.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- server.go
+++ server.go
@@ -3,9 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+var Receipts = make(map[string]Receipt)
+
+type Id struct {
+	Id string `json:"id"`
+}
+
+// Receipt struct)
 type Receipt struct {
 	Retailer     string `json:"retailer"`
 	PurchaseDate string `json:"purchaseDate"`
@@ -13,6 +21,8 @@ type Receipt struct {
 	Total        string `json:"total"`
 	Items        []Item `json:"items"`
 }
+
+// Item struct
 
 type Item struct {
 	Price            string `json:"price"`
@@ -25,7 +35,14 @@ func main() {
 	e.POST("/receipts/process", processReceipt)
 	e.GET("/receipts/:id/points", getPoints)
 
+	// testing
+	e.GET("/receipts", getReceipts)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getReceipts(c echo.Context) error {
+	return c.JSON(http.StatusOK, Receipts)
 }
 
 func getPoints(c echo.Context) error {
@@ -38,7 +55,13 @@ func processReceipt(c echo.Context) error {
 	if err := c.Bind(receipt); err != nil {
 		return err
 	}
-	return c.JSON(http.StatusCreated, receipt)
-	// or
-	// return c.XML(http.StatusCreated, u)
+
+	uuid := uuid.NewString()
+	Receipts[uuid] = *receipt
+
+	// this feels redundant and could be more elegant, but not a priority
+	idResponse := new(Id)
+	idResponse.Id = uuid
+
+	return c.JSON(http.StatusOK, idResponse)
 }
